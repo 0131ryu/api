d7c2773d8cf6bad798cde65e19d56986154f97ac
0131ryu <131ryuji@gmail.com>
2023-04-14 16:09:39 +0900

    사용자 액션, 중첩 쿼리

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const {
@@ -9,21 +10,39 @@ require('dotenv').config();
 const gravatar = require('../util/gravatar');
 
 module.exports = {
-  newNote: async (parent, args, { models }) => {
+  newNote: async (parent, args, { models, user }) => {
+    if (!user) {
+      throw new AuthenticationError('You must be signed in to create a note');
+    }
+
     return await models.Note.create({
       content: args.content,
-      author: 'Adam Scott'
+      author: mongoose.Types.ObjectId(user.id)
     });
   },
-  deleteNote: async (parent, { id }, { models }) => {
+  deleteNote: async (parent, { id }, { models, user }) => {
+    if (!user) {
+      throw new AuthenticationError('You must be signed in to delete a note');
+    }
+    const note = await models.Note.findById(id);
+    if (note && String(note.author) !== user.id) {
+      throw new ForbiddenError("You don't permissions to delete the note");
+    }
     try {
-      await models.Note.findOneAndRemove({ _id: id });
+      await note.remove();
       return true;
     } catch (err) {
       return false;
     }
   },
-  updateNote: async (parent, { content, id }, { models }) => {
+  updateNote: async (parent, { content, id }, { models, user }) => {
+    if (!user) {
+      throw new AuthenticationError('You must be signed in to update a note');
+    }
+    const note = await models.Note.findById(id);
+    if (note && String(note.author) !== user.id) {
+      throw new ForbiddenError("You don't permissions to update the note");
+    }
     try {
       return await models.Note.findOneAndUpdate(
         {
@@ -78,5 +97,45 @@ module.exports = {
 
     // create and return the json web token
     return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  },
+  toggleFavorite: async (parent, { id }, { models, user }) => {
+    if (!user) {
+      throw new AuthenticationError();
+    }
+    let noteCheck = await models.Note.findById(id);
+    const hasUser = noteCheck.favoritedBy.indexOf(user.id);
+
+    if (hasUser >= 0) {
+      return await models.Note.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            favoritedBy: mongoose.Types.ObjectId(user.id)
+          },
+          $inc: {
+            favoriteCount: -1
+          }
+        },
+        {
+          new: true
+        }
+      );
+    } else {
+      //favoriteCount를 1 늘리고 사용자를 목록에 추가
+      return await models.Note.findByIdAndUpdate(
+        id,
+        {
+          $push: {
+            favoritedBy: mongoose.Types.ObjectId(user.id)
+          },
+          $inc: {
+            favoriteCount: 1
+          }
+        },
+        {
+          new: true
+        }
+      );
+    }
   }
 };
--- src/schema.js
+++ src/schema.js
@@ -7,6 +7,8 @@ module.exports = gql`
     id: ID!
     content: String!
     author: User!
+    favoriteCount: Int!
+    favoritedBy: [User!]
     createdAt: DateTime!
     updatedAt: DateTime!
   }
@@ -15,6 +17,7 @@ module.exports = gql`
     id: ID!
     username: String!
     email: String!
+    favorites: [Note!]!
     avatar: String
     notes: [Note!]!
   }
@@ -22,12 +25,16 @@ module.exports = gql`
   type Query {
     notes: [Note!]!
     note(id: ID): Note!
+    user(username: String!): User
+    users: [User!]!
+    me: User!
   }
 
   type Mutation {
     newNote(content: String!): Note
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    toggleFavorite(id: ID!): Note!
     signUp(username: String!, email: String!, password: String!): String!
     signIn(username: String, email: String, password: String!): String!
   }
